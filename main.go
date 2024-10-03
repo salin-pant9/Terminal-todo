f46e81e2ac5d37bf7403c3a0b4bc3018db21999e
salin-pant9 <scorpiopant@gmail.com>
2024-10-03 14:26:55 +0545

    feat:Reading and Writing to file

--- main.go
+++ main.go
@@ -1,102 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
+	"strings"
 )
 
-type Model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
-}
+const fileName = "todo.txt"
 
-func InitialModel() Model {
-	return Model{
-		choices:  []string{"Make a coffee", "Write some code", "Do laundary"},
-		selected: make(map[int]struct{}),
+func WriteToFile() {
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer file.Close()
+	fmt.Println("Write text you want to add:-")
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error loading input", err)
+	}
+	// _, err = fmt.Fprintln(file, text)
+	// if err != nil {
+	// 	fmt.Printf("Error Writing to file: %v\n", err)
+	// }
+	_, err = file.WriteString(text + "\n")
+	if err != nil {
+		fmt.Printf("Error Writing to file: %v\n", err)
+	}
+}
+func ReadFromFile() {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal("Error Reading file", err)
+	}
+	todos := strings.Split(string(data), "\n")
+	for n, todo := range todos {
+		fmt.Printf("%d -> %s", n, todo)
 	}
 }
 
-func (m Model) Init() tea.Cmd {
-	return nil
-}
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	// Is it a key press?
-	case tea.KeyMsg:
-
-		// Cool, what was the actual key pressed?
-		switch msg.String() {
-
-		// These keys should exit the program.
-		case "ctrl+c", "q":
-			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
-		}
-	}
-
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
-	return m, nil
-}
-func (m Model) View() string {
-	// The header
-	s := "What should we buy at the market?\n\n"
-
-	// Iterate over our choices
-	for i, choice := range m.choices {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
-
-	// The footer
-	s += "\nPress q to quit.\n"
-
-	// Send the UI for rendering
-	return s
-}
 func main() {
-	p := tea.NewProgram(InitialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
+
+	fmt.Println("*******MENU **********")
+	// fmt.Println("1.Add a Todo")
+	// fmt.Println("Write text you want to add:-")
+	// reader := bufio.NewReader(os.Stdin)
+	// text, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	fmt.Println("Error loading input", err)
+	// }
+	// fmt.Println(text)
+	// WriteToFile()
+	ReadFromFile()
 }
